d00824aac399b371961896551c5c13847dd8457f
tamjidahmed <mail.to.tamjid@gmail.com>
2024-12-12 02:18:28 +0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
-use std::io::{Read, Write};
 #[allow(unused_imports)]
+use std::io::{BufRead, BufReader, Error, Write};
 use std::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<(), Error>{
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let mut buf = [0; 1024];
-                _ = _stream.read(&mut buf);
-
-                let ss: String = buf.iter().map(|&x| x as char).collect();
-                let ss = ss.split("\r\n").collect::<Vec<&str>>()[0].split_ascii_whitespace().collect::<Vec<&str>>()[1];
-                let mut response = "HTTP/1.1 200 OK\r\n\r\n";
-                if ss != "/" {
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n";  
-                } 
-                _ = _stream.write(response.as_bytes()).unwrap();
+                let reader = BufReader::new(&mut _stream);
+                let line = reader.lines().next().unwrap()?;
+                let var = line.split_ascii_whitespace().nth(1).unwrap();
+                if var == "/" {
+                    let resp = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+                    _stream.write_all(resp.as_bytes())?;
+                    continue;
+                }
+                if !var.starts_with("/echo") {
+                    let resp = "HTTP/1.1 404 Not Found\r\n\r\n".to_string();
+                    _stream.write_all(resp.as_bytes())?;
+                    continue;
+                }
+                let var = &var[6..];
+                let resp = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", var.len(), var);
+                _stream.write_all(resp.as_bytes())?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
