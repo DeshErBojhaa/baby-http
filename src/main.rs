ae7c4702785b8d37f769f75d9c15f2df410d0c46
tamjidahmed <mail.to.tamjid@gmail.com>
2024-12-13 21:55:54 +0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
+use std::io::Read;
 #[allow(unused_imports)]
 use std::io::{BufRead, BufReader, Error, Write};
 use std::net::TcpListener;
 use std::thread;
 
 fn handle_client(mut stream: std::net::TcpStream) {
-    let reader = BufReader::new(&mut stream);
-    let lines: Vec<String> = reader
-        .lines()
-        .map(|line| line.unwrap_or_default())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut reader = BufReader::new(&mut stream);
+    
+    let mut first_line = String::new();
+    reader.read_line(&mut first_line).unwrap();
+    
+    let mut headers = Vec::new();
+    loop {
+        let mut line = String::new();
+        reader.read_line(&mut line).unwrap();
+        if line == "\r\n" || line.is_empty() {
+            break;
+        }
+        headers.push(line);
+    }
 
-    let var = lines[0].split_ascii_whitespace().nth(1).unwrap();
-    let resp = match var {
-        "/" => {
+    let var = first_line.split_ascii_whitespace().nth(1).unwrap();
+    let verb = first_line.split_ascii_whitespace().next().unwrap();
+
+    let resp = match [verb, var] {
+        ["GET", "/"] => {
             "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"
             .to_string()
         }
-        p if p.starts_with("/echo") => {
+        ["GET", p] if p.starts_with("/echo") => {
             let content = &var[6..];
             format!(
                 "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
@@ -25,8 +36,8 @@ fn handle_client(mut stream: std::net::TcpStream) {
                 content
             )
         }
-        "/user-agent" => {
-            let user_agent = lines[lines.len() - 1]
+        ["GET", "/user-agent"] => {
+            let user_agent = headers[headers.len() - 1]
                 .split_ascii_whitespace()
                 .nth(1)
                 .unwrap();
@@ -36,7 +47,7 @@ fn handle_client(mut stream: std::net::TcpStream) {
                 user_agent
             )
         }
-        p if p.starts_with("/files") => {
+        ["GET", p] if p.starts_with("/files") => {
             let file_name = &var[7..];
             let directory = std::env::args().nth(2).unwrap_or_else(|| "/tmp/".to_string());
             let file_path = format!("{}{}", directory, file_name);
@@ -52,6 +63,24 @@ fn handle_client(mut stream: std::net::TcpStream) {
                 Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
             }
         }
+        ["POST", p] if p.starts_with("/files") => {
+            let file_name = &var[7..];
+            let directory = std::env::args().nth(2).unwrap_or_else(|| "/tmp/".to_string());
+            let file_path = format!("{}{}", directory, file_name);
+            let content_length = headers.iter()
+                .find(|line| line.starts_with("Content-Length:")).unwrap()
+                .split_ascii_whitespace().nth(1)
+                .and_then(|len| len.parse::<usize>().ok())
+                .unwrap_or(0);
+
+            let mut data = vec![0; content_length];
+            reader.read_exact(&mut data).unwrap();
+
+            match std::fs::write(file_path, data) {
+                Ok(_) => "HTTP/1.1 201 Created\r\n\r\n".to_string(),
+                Err(_) => "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string(),
+            }
+        }
         _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
     };
     stream.write_all(resp.as_bytes()).unwrap();
@@ -62,14 +91,8 @@ fn main() -> Result<(), Error> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream);
-                });
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Ok(stream) => {thread::spawn(move || handle_client(stream));}
+            Err(e) => {println!("error: {}", e);}
         }
     }
     Ok(())
